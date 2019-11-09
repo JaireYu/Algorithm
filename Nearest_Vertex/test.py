6dffb72022080989c441a540e749dc4ffd456eb4
JaireYu <1561840701@qq.com>
2019-11-09 10:32:12 +0800

    Nearest_Vertex_BugFree

--- Nearest_Vertex/test.py
+++ Nearest_Vertex/test.py
@@ -1,8 +1,8 @@
 import random
 for i in range(1, 100):
     f = open("D:\\GithubLocalRepo\\Algorithm\\Nearest_Vertex\\Nearest_Vertex\\Debug\\test{}.txt".format(str(i)), "w")
-    for j in range(0, 50000):
-        f.write(str(random.random()*200))
+    for j in range(0, 200):
+        f.write(str(random.random()*100))
         f.write(" ")
-        f.write(str(random.random()*200))
+        f.write(str(random.random()*100))
         f.write("\n")
