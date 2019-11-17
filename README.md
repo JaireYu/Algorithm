f7adcc37463a17a742276d60ad28adb532e47fae
JaireYu <1561840701@qq.com>
2019-11-17 10:20:49 +0800

    No bug in Red_Black_Tree

--- README.md
+++ README.md
@@ -13,7 +13,10 @@ BestPair temp2 = Get_Nearest_Vertex(vertexset, vertexset_ysort, Mid_Num + 1, end
 ### problem to solve:
 * return *this 执行后自动调用析构函数
 >Watch out that if you try to use return *this; on a function whose return type is Type and not Type&, C++ will try to make a copy of the object and then immediately call the destructor, usually not the intended behaviour. So the return type should be a reference as in your example.(from stackoverflow)
+实际上是这样的: return *this之后会调用拷贝构造函数，将更改拷贝到当前类，然后调用析构函数，问题在于析构函数直接将root之后的内容解体，所以只需要加一个标志位就可以了，return *this之前将该标志位置0，这样就不会析构了。。。return *this允许方法的迭代使用。
 * 初始化后key没有按照BST的顺序排列
+Insert函数寻找插入位置时，确定是父的左右孩子时，不能比较指针，因为左右孩子可能都是nil.
+* 画图可以使用dot语言+Graphviz online
 ### Attention:
 * 成员尽量用指针表示，如果需要其指针值的话，否则成员函数不改变NODE的值，但是可能改变指针值
  
--- Red_Black_Tree/Red_Black_Tree/Red_Black_Tree/源.cpp
+++ Red_Black_Tree/Red_Black_Tree/Red_Black_Tree/源.cpp
@@ -22,20 +22,27 @@ class Red_Black_Tree {
 	Node* root;
 	//Node nil{ 'b', 0, nullptr, nullptr }; //多个参数类成员变量只能这么声明
 	Node* nil = new Node{ -1, 'b' };	//***注意: 这里如果声明NOde nil, 后面的成员函数虽然无法修改nil的值，但有可能改变其地址
+	int flag; //来标记是否应该析构
 public:
 	Red_Black_Tree() {
 		root = nil;
 	}
 	Red_Black_Tree(vector<int> Init_Vector) {
 		root = nil;
+		flag = 1;
 		int len = Init_Vector.size();
 		for (int i = 0; i != len; i++) {
 			Insert(Init_Vector[i]);
 		}
 	}
 	~Red_Black_Tree() {	//为什么return *this 会自动调用析构？？？
-		DistoryTree(root);
-		delete nil;
+		if(flag == 1){
+			DistoryTree(root);
+			delete nil;
+		}
+		else {
+			flag = 1;
+		}
 	}
 	void DistoryTree(Node* root) {
 		if (root != nil) {
@@ -44,13 +51,13 @@ public:
 			delete(root);
 		}
 	}
-	void Insert(int k) {					//BST插入部分综合了空树和正常树的两种情况，让前驱从nil开始，挺巧的
+	Red_Black_Tree Insert(int k) {					//BST插入部分综合了空树和正常树的两种情况，让前驱从nil开始，挺巧的
 		Node* insert = new Node{ k, 'r' };	//新节点置红色
 		Node* former = nil;
 		Node* latter = root;
 		while (latter != nil) {
 			former = latter;
-			if (k < latter->key) {
+			if (k < latter->key) {		
 				latter = latter->left;
 			}
 			else {
@@ -61,7 +68,7 @@ public:
 			root = insert;
 		}
 		else {	//不是空树，处理该节点的父节点指针域
-			if (latter == former->left) {
+			if (k < former->key) {					//注意判断是不是左孩子时，不能使用latter == former->left, 因为可能都是nil造成误判
 				former->left = insert;
 			}
 			else {
@@ -72,7 +79,8 @@ public:
 		insert->left = nil;
 		insert->right = nil;
 		RBInsertFixup(insert);
-		//return *this;
+		flag = 0;
+		return *this;
 	}
 	void RBInsertFixup(Node* z) {	//调整为红黑树
 		while (z->p->color == 'r') {	//最后处理z是根的情况, 是根的情况不会进入循环，若父亲不是r也没必要调整
@@ -151,6 +159,7 @@ public:
 			RBDeleteFixup(x);
 		}
 		delete y;
+		flag = 0;
 		return *this;
 	}
 	Red_Black_Tree Key_Delete(int k) {
@@ -161,13 +170,13 @@ public:
 		return *this;
 	}
 	void RBDeleteFixup(Node* x) {
-		while (x != root || x->color == 'b') {	//只要x仍然为双黑或者不是根就进行下去
+		while (x != root && x->color == 'b') {	//只要x仍然为双黑或者不是根就进行下去
 			if (x == x->p->left) {	//case1 2 3 4
 				Node* sib = x->p->right;
 				if (sib->color == 'r') {//case1: 不会转移到自身，所以下面的条件没有加else
 					sib->color = 'b';
 					x->p->color = 'r';
-					Right_Rotate(x->p);
+					Left_Rotate(x->p);
 					sib = x->p->right;
 				}
 				if (sib->left->color == 'b' && sib->right->color == 'b') { //case2：case2可以自我循环，所以在一个while内执行完case2就应该退出
@@ -197,7 +206,7 @@ public:
 					Right_Rotate(x->p);
 					sib = x->p->left;
 				}
-				if (sib->left->color == 'b' && sib->right->color) { //case6：case6可以自我循环，所以在一个while内执行完case6就应该退出
+				if (sib->left->color == 'b' && sib->right->color == 'b') { //case6：case6可以自我循环，所以在一个while内执行完case6就应该退出
 					x = x->p;
 					sib->color = 'r';
 				}
@@ -276,10 +285,45 @@ public:
 			return root;
 		}
 	}
+	void Paint_Tree() {
+		cout << "nil [label=\"<f0> | <f1> nil | <f2>\"]" << endl;
+		if(root != nil)
+			Paint(root);
+	}
+	void Paint(Node* curnode) {
+		if (curnode->color == 'r')
+			cout << curnode->key << "[label=\"<f0> |<fl> " << curnode->key << "|<f2>\" color = red]" << endl;
+		else
+			cout << curnode->key << "[label=\"<f0> |<fl> " << curnode->key << "|<f2>\"]" << endl;
+		if (curnode->left == nil) {
+			cout << curnode->key << ":f0:sw -> " << "nil" << ":f1" << endl;
+		}
+		else {
+			Paint(curnode->left);
+			cout << curnode->key << ":f0:sw -> " << curnode->left->key << ":f1" << endl;
+		}
+		if (curnode->right == nil) {
+			cout << curnode->key << ":f2:sw -> " << "nil" << ":f1" << endl;
+		}
+		else {
+			Paint(curnode->right);
+			cout << curnode->key << ":f2:sw -> " << curnode->right->key << ":f1" << endl;
+		}
+	}
 };
 int main() {
-	Red_Black_Tree RBT({ 1, 3, 4, 5, 6, 7, 8, 9 });
-	RBT.Insert(2);
-	RBT.Key_Delete(6);
+	vector<int> Init = {1, 2, 3, 4};
+	Red_Black_Tree RBT(Init);
+	vector<int> test = { 397,25,818,440,457,368,10,699,538,261,485,852,580,967,668,60,63,695,517,948,447,511,978,216,75,326,665,719,352,411,378,596,856,905,754,949,824,732,451,157,28,356,561,110,340,657,281,317,122,972,713,465,171,64,486,31,644,973,628,426,367,728,860,509,527,999,78,765,723,480,38,83,361,375,285,528,736,653,71,475,412,960,103,68,95,92,539,79,802,325,235,435,591,849,301,341,716,373,521,225 };
+	for (int i = 0; i != 50; i++) {
+		RBT.Insert(test[i]);
+	}
+	RBT.Paint_Tree();
+	for (int i = 0; i != 25; i++) {
+		RBT.Key_Delete(test[i]);
+	}
+	RBT.Paint_Tree();
+	RBT.Insert(10000).Key_Delete(1);
+	RBT.Paint_Tree();
 	system("pause");
 }
